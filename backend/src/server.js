76c690a8b95169e93d49d421175f5fbfdbdd2776
alanrslima <alanronison.lima@gmail.com>
2019-10-05 11:16:18 -0300

    Adding socket.io in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,41 @@ const cors = require('cors');
 const routes = require('./routes');
 const path = require('path');
 
-const app = express();
+const socketio = require('socket.io');
+const http = require('http');
 
-mongoose.connect('mongodb+srv://user:123@cluster0-svjdh.mongodb.net/aircnc?retryWrites=true&w=majority', {
+const PORT = 3333;
+const MONGO_USER = 'user';
+const MONGO_PASSWORD = '123';
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0-svjdh.mongodb.net/aircnc?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
+})
 
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
-app.listen(3333, () => {
+server.listen(PORT, () => {
   console.log("Running in port 3333");
 });
--- fronted/src/pages/Dashboard/index.js
+++ fronted/src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
+import socketio from 'socket.io-client';
 import { Link } from 'react-router-dom';
 
 import './styles.css'
@@ -7,6 +8,16 @@ import './styles.css'
 export default function Dashboard() {
 
   const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const user_id = localStorage.getItem('user');
+    const socket = socketio('http://localhost:3333', {
+      query: { user_id }
+    });
+    socket.on('booking_request', data => {
+      console.log(data);
+    }); 
+  }, []);
 
   useEffect(() => {
     async function loadSpots() {
